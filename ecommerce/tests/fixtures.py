907db20ee0b39f0943a7f671631bc264b0909187
parmanand007 <parmanandprajapati006@gmail.com>
2024-02-16 13:40:33 +0530

    run first test creating use and testing its super user or not

---
+++ ecommerce/tests/fixtures.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture
+
+def create_admin_user(django_user_model):
+    """
+    Return admin User
+    """
+    return django_user_model.objects.create_superuser("admin","a@a.com","password")
