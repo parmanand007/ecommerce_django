449d438acea467e00e102e9e01d19aa0196400b7
parmanand007 <parmanandprajapati006@gmail.com>
2024-02-17 14:26:08 +0530

    test category write using faker1

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -13,7 +13,8 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Category
     
-    name= fake.lexify(text="cat_name_?????")
+    # name= fake.lexify(text="cat_name_?????") # this will not produce unique
+    name = factory.Sequence(lambda n: "cat_slug_%d" %n)
     slug=fake.lexify(text='cat_slug_?????')
 
 register(CategoryFactory)
