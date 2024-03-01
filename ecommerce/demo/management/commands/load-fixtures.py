bb5e43ad437d343ddc4b06b7bc3ab27e46faf3ba
parmanand007 <parmanandprajapati006@gmail.com>
2024-03-01 16:29:49 +0530

    create and populate product fixture to db(product model)

--- ecommerce/demo/management/commands/load-fixtures.py
+++ ecommerce/demo/management/commands/load-fixtures.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
         call_command("makemigrations")
         call_command("migrate")
         call_command("loaddata","db_admin_fixture.json"),
-        call_command("loaddata","db_category_fixture.json")
+        call_command("loaddata","db_category_fixture.json"),
+        call_command("loaddata","db_product_fixture.json")
         
         
